2f043aa5ce1dfba1f0b01dbd25745f531303ddf9
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-20 05:50:47 -0700

    Throws parsing error if encountered

--- models/pdf_to_json.js
+++ models/pdf_to_json.js
@@ -8,7 +8,9 @@ const pdfFile = resolve("./class_code_ref.pdf");
 const targetJSON = resolve("./parsedPDF.json");
 const pdfParser = new PDFParser();
 
-pdfParser.on('pdfParser_dataError', errData => console.error(errData.parserError));
+pdfParser.on('pdfParser_dataError', errData => {
+  throw errData;
+});
 
 pdfParser.on("pdfParser_dataReady", pdfData => {
   fs.writeFile(targetJSON, JSON.stringify(pdfData, null, 4));
