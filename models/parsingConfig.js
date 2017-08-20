4bfb05f6eb94d06b9fb31f8c14e9cdb15b96aa4b
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-20 11:23:19 -0700

    Added parsing config file to contain configurable values

---
+++ models/parsingConfig.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // target distance from table separating line to row just below it
+  distFromLineToRowBelow: -0.3,
+  // margin of error
+  distMargin: 0.5,
+  // column titles
+  fields: [
+    'ISO_Description',
+    'ISO_CGL',
+    'SIC',
+    'NAICS',
+    'generalDescription',
+    'NCCI',
+    'CA_WC',
+    'DE_WC',
+    'MI_WC',
+    'NJ_WC',
+    'NY_WC',
+    'PA_WC',
+    'TX_WC',
+  ],
+}
