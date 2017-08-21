2af8162cc6b053aeca236cec30ab990a0667c7aa
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-21 12:58:54 -0700

    Create Express app to return JSON of row data

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 const express = require('express');
+const parsePDFPromise = require('./models');
+
+const app = express();
+const port = process.env.PORT || '3000';
+
+parsePDFPromise.then(main);
+
+function main(pdfData) {
+  app.get('/ncci/:value', createDataFetchingMiddleware(pdfData, 'NCCI'));
+  app.get('/naics/:value', createDataFetchingMiddleware(pdfData, 'NAICS'));
+  app.get('/ca_wc/:value', createDataFetchingMiddleware(pdfData, 'CA_WC'));
+  
+  app.listen(port, function () {
+    console.log(`Listening on ${port}`)
+  })
+}
+
+function createDataFetchingMiddleware(data, field) {
+  return function (req, res) {
+    const codes = getCodesByField(data, field, req.params.value);
+    res.set('Content-Type', 'application/json');    
+    res.send(codes);
+  }
+}
+
+function getCodesByField(data, field, value) {
+  return data.filter(item => item[field] === value);
+}
