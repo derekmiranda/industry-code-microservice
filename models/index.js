3347ca69b0f96c4c87e039640896a5b48b0a8662
Derek Miranda <derek.miranda.1992@gmail.com>
2017-08-22 10:45:12 -0700

    Provide default general code when mapping to specific codes

--- models/index.js
+++ models/index.js
@@ -17,16 +17,11 @@ const parsePDFPromise = new Promise((resolve, reject) => {
     // reference for last found general code in case none found for specific codes
     // initialized at first general code
     let recentViableGenCode = generalCodes[0];
-    const genCodesForSpecCode = specificCodes.map((specCode, i) => {
-      const targetGenCode = getGeneralCodeForSpecificCode(generalCodes, specCode);
-      recentViableGenCode = (targetGenCode || recentViableGenCode);
-      // defaults to last general code if no target found
-      return recentViableGenCode;
-    })
-
     const mergedCodeRows = specificCodes.map((specCode, i) => {
       const targetGenCode = getGeneralCodeForSpecificCode(generalCodes, specCode);
-      const merged = Object.assign(specCode, targetGenCode);
+      recentViableGenCode = (targetGenCode || recentViableGenCode);
+
+      const merged = Object.assign({}, recentViableGenCode, specCode);
       const mergedWithoutFormatData = _.omit(merged, ['page', 'bottomLine_y', 'row_y']);
       return mergedWithoutFormatData;
     })
